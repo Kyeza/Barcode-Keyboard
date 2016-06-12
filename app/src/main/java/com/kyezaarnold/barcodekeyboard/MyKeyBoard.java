8ccdd45936ecae018959109289ce33dd3f9408bf
Kyeza Arnold <kyezaarnold63@gmail.com>
2016-06-12 12:18:47 +0300

    Initial commit -4 on Barcode IME App

--- app/src/main/java/com/kyezaarnold/barcodekeyboard/MyKeyBoard.java
+++ app/src/main/java/com/kyezaarnold/barcodekeyboard/MyKeyBoard.java
@@ -2,11 +2,28 @@ package com.kyezaarnold.barcodekeyboard;
 
 import android.content.Context;
 import android.inputmethodservice.Keyboard;
+import android.view.inputmethod.InputMethodManager;
 
 /**
  * Created by Kyeza Arnold on 6/12/2016.
  */
 public class MyKeyBoard extends Keyboard {
+
+    private Key mEnterKey;
+    private Key mSpaceKey;
+
+    /**
+     * Stores the current state of the mode change key. Its width will be dynamically updated to
+     * match the region of {@link #mModeChangeKey} when {@link #mModeChangeKey} becomes invisible.
+     */
+    private Key mModeChangeKey;
+
+    /**
+     * Stores the current state of the language switch key (a.k.a. globe key). This should be
+     * visible while {@link InputMethodManager#//shouldOfferSwitchingToNextInputMethod(IBinder)}
+     * returns true. When this key becomes invisible, its width will be shrunk to zero.
+     */
+    private Key mLanguageSwitchKey;
 
     public MyKeyBoard(Context context, int xmlLayoutResId) {
         super(context, xmlLayoutResId);
